e522868eb97bd992d6b6c98a636641a81e50cd78
ewwgesha91 <ewwgesha@mail.ru>
2024-01-09 18:39:10 +0100

    Add file index1.html

---
+++ hw2.js
@@ -0,0 +1,15 @@
+// Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 2
+const yearRelease = 'Год выпуска первого iPhone - 2007г.'
+alert(yearRelease);
+
+// Задание 3
+const creatorJS = 'Имя создателя языка JavaScript - Брендан Эйх.'
+alert(creatorJS);
+
+// Задание 4
