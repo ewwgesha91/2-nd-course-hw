95101ea204f02e063ace3b436b2f29a9832d5ad6
ewwgesha91 <ewwgesha@mail.ru>
2024-01-15 23:42:59 +0100

    changed files

---
+++ hw4/hw4.js
@@ -0,0 +1,20 @@
+// Задание 1
+let i = 0;
+	while (i < 2) {
+		console.log('Привет!');
+		i++;
+	}
+
+// Задание 2
+let a = 1;
+    while (a <= 5) {
+        console.log(a);
+        a++;
+    }
+
+// Задание 3
+let b = 7;
+    while (b <= 22) {
+        console.log(b);
+        b++;
+    }
