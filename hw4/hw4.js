87949c23f884f75cf301a38ed2233e13d79b320e
ewwgesha91 <ewwgesha@mail.ru>
2024-01-16 16:41:23 +0100

    changed the file hw4.js

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -6,15 +6,36 @@ let i = 0;
 	}
 
 // Задание 2
-let a = 1;
-    while (a <= 5) {
-        console.log(a);
-        a++;
-    }
+for (a = 1; a <=5; a++) {
+    console.log(a);
+}
+
 
 // Задание 3
-let b = 7;
-    while (b <= 22) {
-        console.log(b);
-        b++;
-    }
+for (b = 7; b <= 22; b++) {
+    console.log(b);
+}
+
+// Задание 4
+const salary = {
+    'Коля' : 200,
+    'Вася' : 300,
+    'Петя' : 400
+};
+for (let name in salary) {
+    console.log(`${name} - зарплата ${salary[name]} долларов`);
+}
+
+// Задание 5
+let n = 1000;
+let num = 0;
+while (n >= 50) {
+    n /= 2;
+    num++;
+}
+console.log(`Получилось число ${n}. Количество итераций ${num}`);
+
+// Задание 6
+for (day = 5; day <= 31; day +=7) {
+    console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
+}
