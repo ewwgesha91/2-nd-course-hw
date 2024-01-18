bf2963eb52e5c52f53099a22d26b50a4693de6ab
ewwgesha91 <ewwgesha@mail.ru>
2024-01-18 17:00:06 +0100

    ghanged game_one.js

---
+++ game_one.js
@@ -0,0 +1,13 @@
+function monthNumber() {
+    let date = Number(prompt('Введите номер месяца'));
+    if (date >= 1 && date <=2 || date === 12) {
+        return 'Зима'
+    } else if (date >= 3 && date <= 5) {
+        return 'Весна'
+    } else if (date >= 6 && date <= 8) {
+        return 'Лето'
+    } else if (date >= 9 && date <= 11) {
+        return 'Осень'
+    }
+}
+alert(monthNumber());
