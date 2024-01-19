469289fcc5dd92ba8d0ed6e4f841e43bb80155af
ewwgesha91 <ewwgesha@mail.ru>
2024-01-19 02:14:35 +0100

    changed files

--- game_one.js
+++ game_one.js
@@ -1,13 +1,14 @@
 function monthNumber() {
     let date = Number(prompt('Введите номер месяца'));
     if (date >= 1 && date <=2 || date === 12) {
-        return 'Зима'
+        alert('Зима'); 
     } else if (date >= 3 && date <= 5) {
-        return 'Весна'
+        alert('Весна'); 
     } else if (date >= 6 && date <= 8) {
-        return 'Лето'
+        alert('Лето'); 
     } else if (date >= 9 && date <= 11) {
-        return 'Осень'
+        alert('Осень'); 
+    } else {
+        alert('Неверный ввод'); 
     }
 }
-alert(monthNumber());
