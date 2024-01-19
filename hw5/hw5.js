3cf297983572fdcaf5b958ffdce52186c8f9c701
ewwgesha91 <ewwgesha@mail.ru>
2024-01-19 03:03:40 +0100

    changed hw5.js

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -1,27 +1,28 @@
-// // Задание 1
-//     let a = Number(prompt('Введите первое число'));
-//     let b = Number(prompt('Введите второе число'));
-// function num(a, b) {
-//     if (a < b) {
-//         return a
-//     } else {
-//         return b
-//     }
-// }
-// let result = num(a, b);
-// console.log(result);
+"use strict";
+// Задание 1
+    let a = Number(prompt('Введите первое число'));
+    let b = Number(prompt('Введите второе число'));
+function num(a, b) {
+    if (a < b) {
+        return a
+    } else {
+        return b
+    }
+}
+let result = num(a, b);
+console.log(result);
 
-// // Задание 2
-// let answer = Number(prompt('Введите любое число'));
-// function userAnswer(answer) {
-//     if (answer % 2 == 0) {
-//         return 'Число чётное'
-//     } else {
-//         return 'Число нечётное'
-//     }
-// }
-// let res = userAnswer(answer)
-// console.log(res);
+// Задание 2
+let answer = Number(prompt('Введите любое число'));
+function userAnswer(answer) {
+    if (answer % 2 == 0) {
+        return 'Число чётное'
+    } else {
+        return 'Число нечётное'
+    }
+}
+let res = userAnswer(answer)
+console.log(res);
 
 // Задание 3.1
 let c = Number(prompt('Введите любое число'));
@@ -49,3 +50,50 @@ function sayHi(age) {
     }
 }
 sayHi(age);
+
+// Задание 5
+let e = Number(prompt('Введите первое число'));
+let f = Number(prompt('Введите второе число'));
+function example(e, f) {
+    if (isNaN(e) || isNaN(f)) {
+        return 'Одно или оба значения не являются числом'; 
+    } else {
+        return e * f;
+    } 
+}
+console.log(example(e, f));
+
+// Задание 6
+function cube() {
+    let g = Number(prompt('Введите любое число'));
+    let numberCube = g ** 3;
+    if (g >= 0 && g <= 10) {
+        console.log(`${g} в кубе равняется ${numberCube}`);
+    } else {
+        return 'Переданный параметр не является числом';
+    }
+}
+console.log(cube());
+    
+
+// Задание 7
+function getCircleArea() {
+    return 3.14 * this.radius ** 2;
+}
+function getCirclePerimeter() {
+    return 2 * this.radius * 3.14;
+}
+const circle1 = {
+    radius: 5,
+    getArea: getCircleArea,
+    getPerimeter: getCirclePerimeter,
+};
+const circle2 = {
+    radius: 3,
+    getArea: getCircleArea,
+    getPerimeter: getCirclePerimeter,
+};
+console.log(circle1.getArea);
+console.log(circle1.getPerimeter);
+console.log(circle2.getArea);
+console.log(circle2.getPerimeter);
