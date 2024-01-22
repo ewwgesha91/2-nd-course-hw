804ad7795ed84eda9a6feb3c0186ae5b41c38650
ewwgesha91 <ewwgesha@mail.ru>
2024-01-22 02:21:58 +0100

    changes hw6.js

---
+++ hw6/hw6.js
@@ -0,0 +1,42 @@
+// Задание 1
+const numbs = [1, 5, 4, 10, 0, 3];
+	for (let i = 0; i < numbs.length; i++) {
+		if (numbs[i] == 10) break;
+		console.log(numbs[i]);
+	};
+
+// Задание 2
+
+// Задание 3
+let num = [1, 3, 5, 10, 20];
+num = num.join('');
+console.log(num);
+
+//Задание 4
+
+// Задание 5
+let a = [1, 1, 1];
+a.push(2, 2, 2);
+console.log(a);
+
+// // Задание 6
+// let b = [9, 8, 7, 'a', 6, 5];
+// b = b.sort();
+
+// // console.log(b);
+// console.log(b.pop());
+// console.log(b);
+
+// // Задание 7
+// let c = [9, 8, 7, 6, 5]
+// let answer = prompt('Угадай число');
+// if (answer === el) {
+//     console.log('Угадал');
+// } else {
+//     console.log('Не угадал');
+// }
+
+// Задание 9
+let e = [[1, 2, 3,],[4, 5, 6]];
+e = e.join(',');
+console.log(e);
