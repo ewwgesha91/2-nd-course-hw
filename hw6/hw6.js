b3fe0bbe6ea28003ad5e8477253ad4b2f04d8867
ewwgesha91 <ewwgesha@mail.ru>
2024-01-24 19:59:19 +0100

    changed hw6.js

--- hw6/hw6.js
+++ hw6/hw6.js
@@ -1,8 +1,8 @@
 // Задание 1
 const numbs = [1, 5, 4, 10, 0, 3];
 	for (let i = 0; i < numbs.length; i++) {
-		if (numbs[i] == 10) break;
 		console.log(numbs[i]);
+		if (numbs[i] == 10) break;
 	};
 
 // Задание 2
