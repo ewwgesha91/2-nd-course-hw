b0a41c2eae6858a059574c9345b90b5242d5ea45
ewwgesha91 <ewwgesha@mail.ru>
2024-01-26 20:45:09 +0100

    changed files

---
+++ hw7/hw7.js
@@ -0,0 +1,17 @@
+// Задание 1
+let str = 'Строка';
+console.log(str.toUpperCase());
+
+// Задание 2
+
+// Задание 3
+let num = 32.58884;
+console.log(Math.floor(num), Math.ceil(num), Math.round(num));
+
+// Задание 4
+
+// Задание 5
+function getRandomInt() {
+    return Math.round(Math.random() * 10 + 1);
+}
+console.log(getRandomInt());
